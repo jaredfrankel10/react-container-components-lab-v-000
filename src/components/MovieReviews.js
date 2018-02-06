20cf7239839722fca336b74bb54221c02d81fe87
jared frankel <jaredfrankel10@gmail.com>
2018-02-06 01:08:47 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,6 +1,6 @@
 // Code MovieReviews Here
 import React from 'react';
- 
+
  //write this way, then all variables must be api_specific keys
  const Review = ({ display_title, headline, summary_short}) => {
    return (
@@ -11,7 +11,7 @@ import React from 'react';
      </div>
    )
  }
- 
+
  //MovieReviews takes its prop 'reviews' (which is an array of reviews) from
  //<MovieReviews reviews={this.state.reviews} /> from LatestMovieReviewsContainer
  const MovieReviews = ({ reviews }) => {
@@ -19,13 +19,13 @@ import React from 'react';
      <div className="review-list">{reviews.map(Review)}</div>
    )
  }
- 
+
  MovieReviews.defaultProps = {
    reviews: []
  }
- 
+
  export default MovieReviews;
- 
+
  // const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
  //
  // MovieReviews.defaultProps = {
